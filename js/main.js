af99246686f222f6c5422f44f427b4ad1d84869f
MaryMiu <m.zhivilo@gmail.com>
2020-02-06 00:34:57 +0700

    Вторая попытка

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 'use strict';
-var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-var pins = [];
-var pinTitle = ['Заголовок предложения 1', 'Заголовок предложения 2', 'Заголовок предложения 3', 'Заголовок предложения 4', 'Заголовок предложения 5', 'Заголовок предложения 6', 'Заголовок предложения 7', 'Заголовок предложения 8'];
-var pinPrice = [1200, 3400, 200, 400, 1400, 5000, 12000, 4500];
-var pinType = ['palace', 'flat', 'house', 'bungalo', 'palace', 'flat', 'house', 'bungalo'];
-var pinRooms = [2, 1, 3, 4, 2, 2, 6, 1];
-var pinGuests = [2, 1, 3, 3, 2, 2, 1, 1];
-var pinCheckin = ['12:00', '13:00', '14:00', '12:00', '13:00', '14:00', '12:00', '13:00'];
-var pinCheckout = ['13:00', '14:00', '12:00', '13:00', '14:00', '12:00', '13:00', '12:00'];
-var pinFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var pinDescription = ['строка с описанием 1', 'строка с описанием 2', 'строка с описанием 3', 'строка с описанием 4', 'строка с описанием 5', 'строка с описанием 6', 'строка с описанием 7', 'строка с описанием 8'];
-var pinPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var mapPins = document.querySelector('.map__pins');
-var fragment = document.createDocumentFragment();
 var MAP_WIDTH = 1200;
 var CCORD_X = 130;
 var COORD_Y = 630;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
+var MIN_PRICE = 500;
+var MAX_PRICE = 10000;
+var MIN_GUESTS = 1;
+var MAX_GUESTS = 10;
+var MIN_ROOMS = 1;
+var MAX_ROOMS = 10;
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var pins = [];
+var pinType = ['palace', 'flat', 'house', 'bungalo'];
+var pinFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var mapPins = document.querySelector('.map__pins');
+var fragment = document.createDocumentFragment();
+
 
 function removeClass(el) {
   document.querySelector(el).classList.remove('map--faded');
@@ -27,27 +26,47 @@ function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+function getRandomTitle() {
+  var titles = ['Квартирка в центре', 'Апартаменты рядом с императорским дворцом', 'Студия на Такэсита', 'Эко-Апартаменты в Одайба', 'Чистая квартира с красивым видом на Сумиду', 'Новая квартира, тепло, уютно, 200 этаж', 'Апартаменты над телевизионной башней', 'Уютная квартира рядом с вокзалом'];
+  return titles[getRandomArbitrary(0, titles.length)];
+}
+
+function getRandomDescription() {
+  var description = ['Можно разместить 20 человек! Удачное расположение в центре и близость к техника, постельное белье, полотенца. Чистая ванна.Wi-Fi на всей территории. Можно с животными.', 'Квартира оборудована всем необходимым от постельного белья до современной бытовой техники, а так же имеется безлимитный бесплатный Wi-Fi Интернет. И, извините, мы не говорим по-русски.', 'Новый дом, современный ремонт, идеальная чистота, безупречное белье, есть все для комфортного проживания.', 'Уютная, тёплая квартира с шикарным местоположением. Рядом железнодорожный вокзал и станция метро, супермаркеты и парикмахерские.'];
+  return description[getRandomArbitrary(0, description.length)];
+}
+
+function getRandomTime() {
+  var hour = ['12:00', '13:00', '14:00'];
+  return hour[getRandomArbitrary(0, hour.length)];
+}
+
+function getRandomImage() {
+  var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+  return photos.slice(0, [getRandomArbitrary(1, photos.length + 1)]);
+}
+
 function createPin(index) {
   var pin = {
     'author': {
       'avatar': 'img/avatars/user0' + [index + 1] + '.png'
     },
     'offer': {
-      'title': pinTitle[index],
+      'title': getRandomTitle(),
       'address': function () {
         var addressX = pin.location.x;
         var addressY = pin.location.y;
         return addressX + ', ' + addressY;
       },
-      'price': pinPrice[index],
-      'type': pinType[index],
-      'rooms': pinRooms[index],
-      'guests': pinGuests[getRandomArbitrary(0, pinGuests.length)],
-      'checkin': pinCheckin[index],
-      'checkout': pinCheckout[index],
+      'price': getRandomArbitrary(MIN_PRICE, MAX_PRICE),
+      'type': pinType[getRandomArbitrary(0, pinType.length)],
+      'rooms': getRandomArbitrary(MIN_ROOMS, MAX_ROOMS),
+      'guests': getRandomArbitrary(MIN_GUESTS, MAX_GUESTS),
+      'checkin': getRandomTime(),
+      'checkout': getRandomTime(),
       'features': pinFeatures.slice(0, [getRandomArbitrary(1, pinFeatures.length + 1)]),
-      'description': pinDescription[index],
-      'photos': pinPhotos.slice(0, [getRandomArbitrary(1, pinPhotos.length + 1)]),
+      'description': getRandomDescription(),
+      'photos': getRandomImage(),
     },
 
     'location': {
@@ -74,12 +93,14 @@ function renderPin(elem) {
   fragment.appendChild(pinCloneTemplate);
 }
 
-removeClass('.map');
+(function showPins() {
+  removeClass('.map');
 
-for (var i = 0; i < 8; i++) {
-  var pin = createPin(i);
-  pins.push(pin);
-}
+  for (var i = 0; i < 8; i++) {
+    var pin = createPin(i);
+    pins.push(pin);
+  }
 
-pins.forEach(renderPin);
-mapPins.appendChild(fragment);
+  pins.forEach(renderPin);
+  mapPins.appendChild(fragment);
+})();
