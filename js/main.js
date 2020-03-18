3f148a3fe28d3b77d3002afcdd4237c967f737a2
MaryMiu <m.zhivilo@gmail.com>
2020-03-18 23:37:11 +0700

    Вешает обработчик на карту

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ var mapPins = document.querySelector('.map__pins');
 var fragment = document.createDocumentFragment();
 var counter = makeCounter();
 var map = document.querySelector('.map');
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 
 
 function showMap() {
@@ -144,6 +145,67 @@ function showPins() {
   mapPins.appendChild(fragment);
 }
 
+function selectOfferType(str) {
+  switch (str) {
+    case 'flat':
+      return 'Квартира';
+    case 'bungalo':
+      return 'Бунгало';
+    case 'house':
+      return 'Дом';
+    case 'palace':
+      return 'Дворец';
+    default:
+      throw new Error('Нет таких значений');
+  }
+}
+
+function createFeaturesElem(arr) {
+  var list = document.createDocumentFragment();
+  arr.forEach(function (item) {
+    var elem = document.createElement('li');
+    elem.className = 'popup__feature popup__feature--' + item;
+    list.append(elem);
+  });
+  return list;
+}
+
+function createPhotosElem(arr) {
+  var list = document.createDocumentFragment();
+  arr.forEach(function (item) {
+    var elem = document.createElement('img');
+    elem.className = 'popup__photo';
+    elem.width = 45;
+    elem.height = 40;
+    elem.alt = 'Фотография жилья';
+    elem.src = item;
+    list.append(elem);
+  });
+  return list;
+}
+
+function renderCard(elem) {
+  var cardCloneTemplate = cardTemplate.cloneNode(true);
+  cardCloneTemplate.querySelector('.popup__title').textContent = elem.offer.title;
+  cardCloneTemplate.querySelector('.popup__text--address').textContent = elem.offer.address();
+  cardCloneTemplate.querySelector('.popup__text--price').textContent = elem.offer.price + ' ₽/ночь';
+  cardCloneTemplate.querySelector('.popup__type').textContent = selectOfferType(elem.offer.type);
+  cardCloneTemplate.querySelector('.popup__text--capacity').textContent = elem.offer.rooms + ' комнаты для ' + elem.offer.guests + ' гостей';
+  cardCloneTemplate.querySelector('.popup__text--time').textContent = 'Заезд после ' + elem.offer.checkin + ', выезд до ' + elem.offer.checkout;
+  cardCloneTemplate.querySelector('.popup__features').innerHTML = '';
+  cardCloneTemplate.querySelector('.popup__features').append(createFeaturesElem(elem.offer.features));
+  cardCloneTemplate.querySelector('.popup__description').textContent = elem.offer.description;
+  cardCloneTemplate.querySelector('.popup__photos').innerHTML = '';
+  cardCloneTemplate.querySelector('.popup__photos').append(createPhotosElem(elem.offer.photos));
+  cardCloneTemplate.querySelector('.popup__avatar').src = elem.author.avatar;
+  fragment.appendChild(cardCloneTemplate);
+}
+
+function showCards() {
+  renderCard(pins[0]);
+  map.appendChild(fragment);
+}
+
 var formNotice = document.querySelector('.ad-form');
 var formMap = document.querySelector('.map__filters');
 
@@ -180,6 +242,7 @@ function deactivate() {
 
 function activate() {
   showPins();
+  showCards();
   enableAllForms();
   formNotice.classList.remove('ad-form--disabled');
 }
@@ -354,3 +417,15 @@ function setAddressInputValue() {
 }
 
 setAddressInputValue();
+
+mapPins.addEventListener('click', pinClickHandler);
+
+function pinClickHandler(evt) {
+  var currentPin = evt.target.closest('.map__pin');
+  if (evt.target.closest('.map__pin--main')) {
+    return;
+  }
+  if (!currentPin) {
+    return;
+  }
+}
